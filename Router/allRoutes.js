5cc5813c0f75b47fa1ba97906ee2661ba873ac76
cyril andriot <cyrilandriot0232010@gmail.com>
2024-06-01 23:15:54 +0200

    add page connexion et inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,11 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/robe", "Sélection de robe", "/pages/robe.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
