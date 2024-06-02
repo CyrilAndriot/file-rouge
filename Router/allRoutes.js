72b08a95396c4221dd5722eafc0747accaae146f
cyril andriot <cyrilandriot0232010@gmail.com>
2024-06-02 22:49:15 +0200

    gestion de l'affichage reservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,6 +8,8 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/compte", "Mon Compte", "/pages/auth/compte.html"),
     new Route("/editPassword", "Modifier votre mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos Réservation", "/pages/reservation/allResa.html"),
+    new Route("/allResa", "Vos Réservation", "/pages/reservation/allResa.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
