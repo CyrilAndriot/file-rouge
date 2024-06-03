85218d65c33783c4c5b6a3e568b8ff89524e0a9c
cyril andriot <cyrilandriot0232010@gmail.com>
2024-06-03 22:15:36 +0200

    vérifier les champs requis

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,11 +5,11 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/robe", "Sélection de robe", "/pages/robe.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", "/js/auth/signup.js"),
     new Route("/compte", "Mon Compte", "/pages/auth/compte.html"),
     new Route("/editPassword", "Modifier votre mot de passe", "/pages/auth/editPassword.html"),
     new Route("/allResa", "Vos Réservation", "/pages/reservation/allResa.html"),
-    new Route("/allResa", "Vos Réservation", "/pages/reservation/allResa.html"),
+    new Route("/reserver", "Réserver", "/pages/reservation/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
